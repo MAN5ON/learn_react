829d364d512854508d0f10e363dfe7fad63521ef
MAN5ON <dark34rus@mail.ru>
2022-05-18 19:14:43 +0300

    feat(global): create state

--- src/components/content/profile/profile.jsx
+++ src/components/content/profile/profile.jsx
@@ -6,14 +6,7 @@ import { Description } from './userInfo/description/description';
 import { ProfileHeadPic } from './userInfo/profileHeader/profileHeader';
 import { ProfilePic } from './userInfo/profilePic/profilePic';
 
-const posts = [
-    {id: 1, message: '1 post', likes: 13},
-    {id: 2, message: '2 post', likes: 9},
-    {id: 3, message: '3 post', likes: 12},
-    {id: 4, message: '4 post', likes: 29},
-]
-
-export const Profile = () => {
+export const Profile = (posts) => {
     return (
         <div>
             <div>
@@ -27,11 +20,10 @@ export const Profile = () => {
                 <NewPost />
                 {
                     posts.map(post => (
-                        <ItemPost key={post.id} message={post.message} likes= {post.likes}/>
+                        <ItemPost key={post.id} message={post.message} likes={post.likes} />
                     ))
                 }
             </div>
         </div>
     )
 }
-
