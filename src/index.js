d2c6831f30c95f95f25670246a5f04ddc86bc834
MAN5ON <dark34rus@mail.ru>
2022-05-18 19:07:13 +0300

    feat(global): create state

--- src/index.js
+++ src/index.js
@@ -3,27 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
-
-const diaItems = [
-  { id: 1, name: 'Renat Bikmukhamedov', message: 'Ебать пропсы работают наконец-то' },
-  { id: 2, name: 'Vlad Serebrov', message: 'Всё хуйня, учи хуки' },
-  { id: 3, name: 'Misha Lepilin', message: 'Всё хуйня, пошли пить пиво' },
-  { id: 4, name: 'Sasha Volobueva', message: 'Ты хуйня' },
-  { id: 5, name: 'Misha Amazyan', message: '"Что-то на Армянском"' },
-  { id: 6, name: 'Max Morkovkin', message: 'Курю скальпель' },
-  { id: 7, name: 'Sasha Lepilin', message: 'go dota 2' },
-]
-
-const posts = [
-  {id: 1, message: '1 post', likes: 13},
-  {id: 2, message: '2 post', likes: 9},
-  {id: 3, message: '3 post', likes: 12},
-  {id: 4, message: '4 post', likes: 29},
-]
+import { state } from './components/Redux/State'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App diaItems={diaItems} posts={posts}/>
+    <App diaItems={state.diaItems} posts={state.posts}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
